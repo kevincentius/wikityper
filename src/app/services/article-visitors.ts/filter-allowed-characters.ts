8f8e0ccd5a14636ab005c3e01044c1cdd6027d28
kevincentius <kevincentius@yahoo.com>
2023-02-16 23:24:28 +0100

    initial commit - problem with foreign characters

---
+++ src/app/services/article-visitors.ts/filter-allowed-characters.ts
@@ -0,0 +1,8 @@
+import { Article } from "src/app/model/article";
+
+export function filterAllowedCharacters(article: Article) {
+  for (let section of article.sections) {
+    section.paragraphs = section.paragraphs.filter(p => p.fullText.trim().length > 0);
+  }
+  article.sections = article.sections.filter(s => s.paragraphs.length > 0);
+}
